273b488c00065835a87eb33142d43ee5ed8d9b21
Alejandro Medina <5almero5@gmail.com>
2016-03-19 12:55:14 -0600

    Add Controler and items

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 if (Meteor.isClient) {
     angular.module('officefood', ['angular-meteor']);
+
+      angular.module('officefood').controller('menuList', ['$scope', function ($scope) {
+      $scope.foods = [
+      {
+        'name': 'Dubstep-Free Zone',
+        'description': 'Can we please just for an evening not listen to dubstep.'
+      },
+      {
+        'name': 'All dubstep all the time',
+        'description': 'Get it on!'
+      },
+      {
+        'name': 'Savage lounging',
+        'description': 'Leisure suit required. And only fiercest manners.'
+      }
+    ];
+  }]);
+
+
 }
