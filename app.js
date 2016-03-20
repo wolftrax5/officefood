5240f5c34b0f62b7ccb67be6d0d2c487648448dd
Hazael Vidales <hazael.vidales@gmail.com>
2016-03-19 23:10:31 -0600

    Add more routing for basic flow

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ if (Meteor.isClient) {
     angular.module('officefood').config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
         $locationProvider.html5Mode(true);
         $stateProvider
+            .state('home', {
+                url: '/home',
+                template: '<home></home>'
+            })
             .state('menu', {
                 url: '/menu',
                 template: '<menu-list></menu-list>'
@@ -11,13 +15,33 @@ if (Meteor.isClient) {
             .state('food', {
                 url: '/food/:foodId',
                 template: '<food-details></food-details>'
+            })
+            .state('shoppingcart', {
+                url: '/shoppingcart',
+                template: '<shooping-cart><shopping-cart/>'
+            })
+            .state('subscribe', {
+                url: 'subscribe',
+                template: '<subscribe></subscribe>'
             });
         $urlRouterProvider.otherwise("/menu");
     });
+
+    angular.module('officefood').directive('home', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'template/home.html',
+            controllerAs: 'home',
+            controller: function($scope, $reactive) {
+                // TODO /home
+            }
+        };
+    });
+
     angular.module('officefood').directive('menuList', function () {
         return {
             restrict: 'E',
-            templateUrl: 'template/menu-list.html',
+            templateUrl: 'template/menu.html',
             controllerAs: 'menuList',
             controller: function ($scope, $reactive) {
                 $reactive(this).attach($scope);
@@ -37,18 +61,42 @@ if (Meteor.isClient) {
             }
         }
     });
+    angular.module('officefood').directive('shoppingCart', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'template/cart.html',
+            controllerAs: 'shoopingCart',
+            controller: function($scope, $reactive) {
+                // TODO /shopping-cart
+            }
+        };
+    });
+
+    angular.module('officefood').directive('subscribe', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'template/subscribe.html',
+            controllerAs: 'subscribe',
+            controller: function($scope, $reactive) {
+                // TODO /subscribe
+            }
+        };
+    });
+
     angular.module('officefood').directive('foodDetails', function () {
         return {
             restrict: 'E',
-            templateUrl: 'template/food-details.html',
+            templateUrl: 'template/food.html',
             controllerAs: 'foodDetails',
             controller: function ($scope, $stateParams, $reactive) {
                 $reactive(this).attach($scope);
-                this.helpers({
-                    food: function () {
-                        return Foods.findOne({_id: $stateParams.foodId});
-                    }
-                });
+                this.food = Foods.findOne({_id: $stateParams.foodId});
+                //this.helpers({
+                //    food: function () {
+                //        return Foods.findOne({_id: $stateParams.foodId});
+                //    }
+                //});
+
                 this.save = function () {
                     Foods.update({
                             _id: $stateParams.foodId
